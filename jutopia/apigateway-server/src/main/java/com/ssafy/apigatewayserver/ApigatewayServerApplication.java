a35c8400916cb9fd20b8a55ec57db4740a0761b9
Young Seob So <9412sys@gmail.com>
2023-09-11 15:35:18 +0900

    feat: initial commit
    Former-commit-id: 44fabf4568617595053a1f084e16d443bc348caf

---
+++ jutopia/apigateway-server/src/main/java/com/ssafy/apigatewayserver/ApigatewayServerApplication.java
@@ -0,0 +1,14 @@
+package com.ssafy.apigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ApigatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApigatewayServerApplication.class, args);
+    }
+
+}
