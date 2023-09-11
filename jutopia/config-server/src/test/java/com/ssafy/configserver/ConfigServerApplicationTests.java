5d38c36a5c514b5df890414fc6de1579bd4e2130
Young Seob So <9412sys@gmail.com>
2023-09-11 15:35:18 +0900

    feat: initial commit

---
+++ jutopia/config-server/src/test/java/com/ssafy/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7e948f227b7e9e8042887335236a9ab4a29967bd9da3616684a6dcfbc3d66e14
+size 229
---
+++ jutopia/src/main/java/com/ssafy/jutopia/JutopiaApplication.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e63fe27873c74245fc8eb4bfa65076de866b600ae9d0d2905712d19308ea4885
+size 324
