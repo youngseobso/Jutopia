e5ea463f5dfdcd328680548de9cd8754b69247d0
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-09-12 12:10:52 +0900

    🍱 feat: 뉴스 서비스 초기 업로드
    Eureka, MongoDB 연결까지 완료
    
    
    Former-commit-id: a56aa34baa4e8af92ba792a3383486c7253e965e

---
+++ jutopia/news-service/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:31d8ecc0ec3d3af6e8117b7aedf133cae7eda732bd41aa03329c6ce05de05aaa
+size 241
---
+++ jutopia/news-service/main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ddc0977481fbce3c92b480f9af0c20b8c7108e7a38dbabe96308defff4f3b2f7
+size 2236
