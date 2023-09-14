ef337b53c4b3c8797bebe1000cedeff6606bc5c8
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-09-14 15:57:00 +0900

    🐳 chore: news-server Dockerfile 작성
    Former-commit-id: 7c5b8b49dec93544aab452b333e7f7effe430665

---
+++ jutopia/news-server/main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ec7ce59bbdec11fce52c64cad9c9389cd8b232dfdd10f155926c02deec8c284a
+size 2238
--- jutopia/news-service/main.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ddc0977481fbce3c92b480f9af0c20b8c7108e7a38dbabe96308defff4f3b2f7
-size 2236
