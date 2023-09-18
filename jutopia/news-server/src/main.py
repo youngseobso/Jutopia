a84bd8e1cbf7de25b5b3d1ad06f5d3e4ba221554
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-09-18 11:06:02 +0900

    🍃 chore: MongoDB DB 이름, collection 이름 변경
    Former-commit-id: 30dd7b47c3787a41373cb346c1a13eed440f7333

--- jutopia/news-server/src/main.py
+++ jutopia/news-server/src/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90fd466eb0814331b27cc008c2d1ac53ca5baa1e494a567aa0345fa14fa492d8
-size 2241
+oid sha256:fe7dee8f550eff7bb34eed017fb1d1debcff29b86254dfadf9f3b8d3fe8a8d72
+size 2225
