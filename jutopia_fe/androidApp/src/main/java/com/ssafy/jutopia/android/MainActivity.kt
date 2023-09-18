1292e4ab74082f7061a9372cb614837d45d0b8b3
김현종 <dw3510@naver.com>
2023-09-18 09:42:16 +0900

    Feat : KMM base project 구성
    Former-commit-id: 3adf732485eca582392fa4111e03a0188f41a8de

---
+++ jutopia_fe/androidApp/src/main/java/com/ssafy/jutopia/android/MainActivity.kt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cff6a4b9babc776ef6b18136e7133feb010e88acbf120a129c0ea0a68c14d828
+size 1049
---
+++ jutopia_fe/androidApp/src/main/java/com/ssafy/jutopia/android/MyApplicationTheme.kt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5e58995396738eb4720e1094777cf0bcd91bed2ad76f1d1af9b043fd4bbabe7c
+size 1652
