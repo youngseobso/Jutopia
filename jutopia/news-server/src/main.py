0017651b833aba3ee461a7755f0d900e27c92b51
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-09-18 15:34:27 +0900

    🚑 fix: URL 주소 오류 수정
    Former-commit-id: aade954e85be6b8a6822a10ac8cdfe438f91f54d

--- jutopia/news-server/src/main.py
+++ jutopia/news-server/src/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:903371b0b2ac01c9bd7303acce924221d76d4c824282850105a21cda6c6c603b
-size 2318
+oid sha256:c9e93b3788eb433e080055b5e54a689ef86a691f372cbc28a68c7e35dd237444
+size 2492
