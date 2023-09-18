8b8151e3368cf5bff5a68cb045416dbe83489456
kihong2424 <kihong2424@gmail.com>
2023-09-18 08:18:21 +0000

    🚀 chore: 포트번호 9001로 이동
    Former-commit-id: 0cf64bc2e209ceec3270224fe1d056b9a6c3078c

--- jutopia/news-server/src/main.py
+++ jutopia/news-server/src/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:903371b0b2ac01c9bd7303acce924221d76d4c824282850105a21cda6c6c603b
-size 2318
+oid sha256:9acaa4bf3feed74acfa0dff86c8cda153a243334bbd6778acca496527d88c314
+size 2492
