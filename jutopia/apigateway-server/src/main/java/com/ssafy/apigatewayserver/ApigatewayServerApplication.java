ac64735af4b5edfc02ec654a1e8c601b8a74bba1
신성주 <hans52410537@gmail.com>
2023-09-18 17:53:22 +0900

    📝Fix: fix yaml file
    Former-commit-id: 9aa2676c34ab7f8f8c9c4daf1514f0ccf9ec13b6

--- jutopia/apigateway-server/src/main/java/com/ssafy/apigatewayserver/ApigatewayServerApplication.java
+++ jutopia/apigateway-server/src/main/java/com/ssafy/apigatewayserver/ApigatewayServerApplication.java
@@ -1,3 +1,14 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b5cec19264308aaa517eb736d476dc8327823b45ec54cf848e9925a61e4d8b8a
-size 403
+package com.ssafy.apigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApigatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApigatewayServerApplication.class, args);
+    }
+
+}
