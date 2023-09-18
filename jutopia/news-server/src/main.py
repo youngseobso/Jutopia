0ea4259a3d67140a81fe57e607dd584876f26cb1
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-09-18 14:11:30 +0900

    🍃 chore: news-server 인스턴스 호스트 변경
    Former-commit-id: 415a11bd1686a61a22cf9bf16aaa48cead1da234

--- jutopia/news-server/src/main.py
+++ jutopia/news-server/src/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe7dee8f550eff7bb34eed017fb1d1debcff29b86254dfadf9f3b8d3fe8a8d72
-size 2225
+oid sha256:903371b0b2ac01c9bd7303acce924221d76d4c824282850105a21cda6c6c603b
+size 2318
