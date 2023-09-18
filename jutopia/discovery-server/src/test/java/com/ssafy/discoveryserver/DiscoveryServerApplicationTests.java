3d97931c341fe78a97aa986390c3f0a30e285e03
kihong2424 <kihong2424@gmail.com>
2023-09-18 08:23:06 +0000

    🚧 chore: news-server 연결되게 작업중
    Former-commit-id: 71de0b716a2fded924bd18f47289d016159b86a0

--- jutopia/discovery-server/src/test/java/com/ssafy/discoveryserver/DiscoveryServerApplicationTests.java
+++ jutopia/discovery-server/src/test/java/com/ssafy/discoveryserver/DiscoveryServerApplicationTests.java
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:109b6d9cc904f2d8afbd26f6382106e05bbfd27d740c36624268fb5c0916eee0
-size 235
+package com.ssafy.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
