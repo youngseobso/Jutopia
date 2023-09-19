51aca077fc7b8d9f192c9159d321fd10cc2460df
신성주 <hans52410537@gmail.com>
2023-09-20 00:31:23 +0900

    📝Feat: get user accounts
    Former-commit-id: 2a9057cf3ffca90a132894163f9bdf092f1d7252

--- jutopia/bank-server/src/main/java/com/ssafy/bankserver/controller/BankController.java
+++ jutopia/bank-server/src/main/java/com/ssafy/bankserver/controller/BankController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d18d664ca263652dc7fa60e92c58ec59486f298322d5a48f4b392f6318ea22a5
-size 582
+oid sha256:dde8a4a30194aefb6f40ebf61bd4f5e2ccbfd18d84a5856d714e2c7976443f39
+size 1134
