b903ec46e940e513b1975917a93d1939e27f4fdf
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-09-19 16:33:08 +0900

    🍃 chore: mongoDB replica set 구성 변경
    Former-commit-id: 1ba8acc9136911f801e841cd006debeb3576280f

--- jutopia/common/src/main/java/com/ssafy/common/CommonApplication.java
+++ jutopia/common/src/main/java/com/ssafy/common/CommonApplication.java
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:aa81d9603d43bdd4832f0dcba4d4946b77ec19f7d479d3cc6c449c67af75e859
-size 321
+package com.ssafy.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonApplication.class, args);
+    }
+
+}
--- jutopia/common/src/test/java/com/ssafy/common/CommonApplicationTests.java
+++ jutopia/common/src/test/java/com/ssafy/common/CommonApplicationTests.java
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:42c27b2b938d9f781026e36594f36564ee9378b5d745f2d2dd6273aaadd4e2cb
-size 217
+package com.ssafy.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
