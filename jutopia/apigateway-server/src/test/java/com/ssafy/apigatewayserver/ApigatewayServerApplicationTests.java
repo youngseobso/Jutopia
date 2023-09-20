28d71d755c5e62dc2868c0648ae0f6c1ba5a5a2e
kihong2424 <kihong2424@gmail.com>
2023-09-20 15:52:39 +0900

    📝 chore: docker-db 디렉토리를 redis-server 로 이름 변경
    Former-commit-id: fae7a12586fa5259100ad6c64ddfcaf076a9fe16

--- jutopia/apigateway-server/src/test/java/com/ssafy/apigatewayserver/ApigatewayServerApplicationTests.java
+++ jutopia/apigateway-server/src/test/java/com/ssafy/apigatewayserver/ApigatewayServerApplicationTests.java
@@ -1,13 +1,3 @@
-package com.ssafy.apigatewayserver;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ApigatewayServerApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:e96d1731840705d3ed61fad9ff98ad8f86d178899534b36331bd5dcea3d82a30
+size 237
