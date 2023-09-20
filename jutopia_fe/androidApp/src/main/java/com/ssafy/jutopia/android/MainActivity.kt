38abdbdada9af4a12eced2b8357ef897c4e83fd2
im jun hwan <dlawnsghks12@naver.com>
2023-09-20 10:43:43 +0900

    📝Feat: create main_menu_button
    Former-commit-id: 9dac804b0f96268ef95e80a9fcdb4c5798b3ce20

--- jutopia_fe/androidApp/src/main/java/com/ssafy/jutopia/android/MainActivity.kt
+++ jutopia_fe/androidApp/src/main/java/com/ssafy/jutopia/android/MainActivity.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cff6a4b9babc776ef6b18136e7133feb010e88acbf120a129c0ea0a68c14d828
-size 1049
+oid sha256:a7bb134e49efc7f13706a91d72a59886d8192d9273335f5df165cad101a9d785
+size 1380
