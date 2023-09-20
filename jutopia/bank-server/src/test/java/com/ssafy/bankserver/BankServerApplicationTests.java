05baa738748723cb2b0d18cb072ac60b50f271de
im jun hwan <dlawnsghks12@naver.com>
2023-09-20 10:43:43 +0900

    📝Feat: create main_menu_button
    Former-commit-id: c2071affe34107834683e9910b450b895943d3c7

--- jutopia/bank-server/src/test/java/com/ssafy/bankserver/BankServerApplicationTests.java
+++ jutopia/bank-server/src/test/java/com/ssafy/bankserver/BankServerApplicationTests.java
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0c60430c19a5222eb3fcf3ec7711b2e9be2156724839769f157abd486a53f85c
-size 216
+package com.ssafy.bankserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
