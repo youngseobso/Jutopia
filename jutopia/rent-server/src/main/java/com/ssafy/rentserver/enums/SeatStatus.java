080fc0f9e9a21995dddb37b9dfc8b930c57c099b
bellring <starringbell@gmail.com>
2023-09-22 00:39:45 +0900

    Feat : rent-server redis 설정
    Former-commit-id: 936cfe5c64ae1b0851553778bad4e29713a866f9

--- jutopia/rent-server/src/main/java/com/ssafy/rentserver/enums/SeatStatus.java
+++ jutopia/rent-server/src/main/java/com/ssafy/rentserver/enums/SeatStatus.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public enum SeatStatus {
-    POSSIBLE,
-    IMPOSSIBLE;
+    AVAILABLE("사용가능"),
+    UNAVAILABLE("사용불가"),
+    INUSE("사용중");
+
+    private final String description;
 }
