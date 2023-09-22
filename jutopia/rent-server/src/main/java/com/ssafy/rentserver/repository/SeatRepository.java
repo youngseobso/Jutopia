11ce38be3a0980282ecc6ff3e46a072ba5fd6e01
김현종 <dw3510@naver.com>
2023-09-22 17:47:59 +0900

    Feat : 전체 좌석 조회 기능 추가
    Former-commit-id: aaf100927ebe425d499b723a4d65659a52ee2679

--- jutopia/rent-server/src/main/java/com/ssafy/rentserver/repository/SeatRepository.java
+++ jutopia/rent-server/src/main/java/com/ssafy/rentserver/repository/SeatRepository.java
@@ -1,13 +1,3 @@
-package com.ssafy.rentserver.repository;
-
-import com.ssafy.rentserver.model.Seat;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-import java.util.UUID;
-
-public interface SeatRepository extends JpaRepository<Seat, String> {
-
-    Optional<Seat> findById(UUID seatId);
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:7b3a2e571a477e68c7ab58c1d55c303845bc180838506c524a1d17a9c96068f1
+size 674
