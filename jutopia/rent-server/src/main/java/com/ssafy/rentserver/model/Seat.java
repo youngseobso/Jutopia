c6a0317f9b82613f8104796a40d3288ef90f758a
김현종 <dw3510@naver.com>
2023-09-22 17:47:59 +0900

    Feat : 전체 좌석 조회 기능 추가
    Former-commit-id: b46d147f8adaa8d7016f47fed4506ea4f89f77e8

--- jutopia/rent-server/src/main/java/com/ssafy/rentserver/model/Seat.java
+++ jutopia/rent-server/src/main/java/com/ssafy/rentserver/model/Seat.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ef262f73e858f7e9d6b13e2092e1b787003df619bbb8e3a1322895546fc85df
-size 1248
+oid sha256:24f6c3ba4cc5e48547740b23adb1d5023df831aad70c220b6b5527e886a94476
+size 1305
--- jutopia/rent-server/src/main/java/com/ssafy/rentserver/repository/SeatRepository.java
+++ jutopia/rent-server/src/main/java/com/ssafy/rentserver/repository/SeatRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7947ba7a565a28aa69661aedae1cdccf909b63236dc756f9724c96ba448bb5a7
-size 312
+oid sha256:7b3a2e571a477e68c7ab58c1d55c303845bc180838506c524a1d17a9c96068f1
+size 674
