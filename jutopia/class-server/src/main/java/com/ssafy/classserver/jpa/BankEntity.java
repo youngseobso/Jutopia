c590c6303233460e7a8dca71cb958660b5c8ada0
신성주 <hans52410537@gmail.com>
2023-09-23 16:21:04 +0900

    🎨Rename: bank 서버, class 서버 합병
    Former-commit-id: 7b0cd07cb60193244245b8371a61ec76c88f600c

---
+++ jutopia/class-server/src/main/java/com/ssafy/classserver/jpa/BankEntity.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5aa57ef1523619104da4ddfccf7337580a7e11338f8e0b8ee9de63a692fbc36b
+size 860
---
+++ jutopia/class-server/src/main/java/com/ssafy/classserver/jpa/SavingProductsEntity.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ffa3b22a423029f8a8360269ac9f1e89dfaa4be0db5e34e40e2773ea35388fb9
+size 649
