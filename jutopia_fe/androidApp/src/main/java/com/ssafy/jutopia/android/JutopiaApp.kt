1ce309b2056a19328ae3a1d52491b3bbbf731b2d
bellring <starringbell@gmail.com>
2023-09-25 05:29:59 +0900

    Feat : 임대 UI 구성
    Former-commit-id: 727e2863214cc5383b2d6f514155fe28473e618d

--- jutopia_fe/androidApp/src/main/java/com/ssafy/jutopia/android/JutopiaApp.kt
+++ jutopia_fe/androidApp/src/main/java/com/ssafy/jutopia/android/JutopiaApp.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd6ad76d28ec89a217e2ff91f4cb3d8fb9518fc653581e1380622b39d78ed9cb
-size 6383
+oid sha256:827131eebe06a262b9a5aec5cb5001e39ed57df939f5a00c855a6e43dc826aa6
+size 6389
