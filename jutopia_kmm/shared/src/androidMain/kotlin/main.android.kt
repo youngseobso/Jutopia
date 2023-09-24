6e68d8bcd95b325b39a1e79b78007b7fdbbd6f47
dhkim6956 <dhkim6956@naver.com>
2023-09-25 07:07:25 +0900

    💄Design: add common FontFamily
    Former-commit-id: 960c99e4676e31a2f48273ea932d15ff03c84680

--- jutopia_kmm/shared/src/androidMain/kotlin/main.android.kt
+++ jutopia_kmm/shared/src/androidMain/kotlin/main.android.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e556663c1da05a2450005188749e19b2a5c96fa10664709ac12663c25e3da14
-size 404
+oid sha256:fe5b9804cd1b456d060cf7b1e098dd68b4dcfaa0ba87d8d7242e5447d67824b4
+size 683
