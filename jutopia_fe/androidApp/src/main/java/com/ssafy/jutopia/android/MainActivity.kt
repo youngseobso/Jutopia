d5c77ffa93ff76b58c993ac23286187ea82a0dcb
bellring <starringbell@gmail.com>
2023-09-25 03:41:31 +0900

    Feat : Main Page 구성
    Former-commit-id: 890b3dbedf019460d897c10124670ef8dc79224b

--- jutopia_fe/androidApp/src/main/java/com/ssafy/jutopia/android/MainActivity.kt
+++ jutopia_fe/androidApp/src/main/java/com/ssafy/jutopia/android/MainActivity.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7bb134e49efc7f13706a91d72a59886d8192d9273335f5df165cad101a9d785
-size 1380
+oid sha256:10487afa0892232fad7b7232971e9ff923c5bfd858a09afa9326a90210ee4887
+size 571
---
+++ jutopia_fe/androidApp/src/main/java/com/ssafy/jutopia/android/ui/LeasePage.kt
@@ -0,0 +1,9 @@
+package com.ssafy.jutopia.android.ui
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LeasePage() {
+    Text("임대페이지")
+}
