022662b83535d5ca2d5912797f51d80952fb3b43
Young Seob So <9412sys@gmail.com>
2023-09-25 15:23:02 +0900

    feat: 📰create stock-server
    Former-commit-id: dd54693687fb1482d3cb98450780783ea5d5c70f

---
+++ jutopia/stock-server/src/test/java/com/ssafy/zutopia/stockserver/StockServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.zutopia.stockserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
