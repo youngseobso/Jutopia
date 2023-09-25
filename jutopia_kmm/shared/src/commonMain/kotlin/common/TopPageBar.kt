acb03e8e0f8db0192af67da4ce42e60feb8b4161
dhkim6956 <dhkim6956@naver.com>
2023-09-26 02:55:43 +0900

    📝Feat: return button
    Former-commit-id: 1054ecdee446091f4a72e4fa89d61c5bbe6f3270

--- jutopia_kmm/shared/src/commonMain/kotlin/common/TopPageBar.kt
+++ jutopia_kmm/shared/src/commonMain/kotlin/common/TopPageBar.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f4497041b754924b9e12b5c5030f37de61ebc7e8690394d2c71106b9f5ef4eb
-size 1418
+oid sha256:da2425d11e4ec47b360c50e3d8f634a4f3a9b421169f7b86d0614e0819722e03
+size 2096
