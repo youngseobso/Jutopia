0f1ec8222e852bd83e9b612c304b11069a4dbf08
김현종 <dw3510@naver.com>
2023-09-25 12:34:37 +0900

    Feat : Log 추적을 위한 디펜던시 추가
    Former-commit-id: df83b1e37486a0c094910ee29e145cfdacb0304d

--- jutopia_kmm/shared/build.gradle.kts
+++ jutopia_kmm/shared/build.gradle.kts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8de96e50ab9b26e0f3f867e31548128bd9d111b5e4bcb8d4e1e3679756e05add
-size 2367
+oid sha256:3837c4cafd177a0331cb2c522f4f0b0e3b370b30e5de3c28c58656c84fe02b07
+size 2426
