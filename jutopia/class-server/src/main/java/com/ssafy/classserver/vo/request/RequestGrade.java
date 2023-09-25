c25ad56191bb8762b94136eaaf6cab1c36fff02c
신성주 <hans52410537@gmail.com>
2023-09-25 21:17:43 +0900

    📰Fix: gradle
    Former-commit-id: b14415e14b9ae2b67cbd8bd357c3289898655ed9

--- jutopia/class-server/src/main/java/com/ssafy/classserver/vo/request/RequestGrade.java
+++ jutopia/class-server/src/main/java/com/ssafy/classserver/vo/request/RequestGrade.java
@@ -1,16 +1,3 @@
-package com.ssafy.classserver.vo.request;
-
-import jakarta.validation.constraints.NotNull;
-import lombok.Data;
-
-import java.math.BigDecimal;
-
-@Data
-public class RequestGrade {
-    @NotNull(message = "학년을 설정 해주세요")
-    private int gradeNum;
-
-    // 학년별 gradeAccountPoint 및 gradeAccountInitPoint 설정
-    private BigDecimal gradeAccountPoint;
-    private BigDecimal gradeAccountInitPoint;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:f170831a2201f02d4c471d96f994d5730dbbbcccdff02d8e561cbb376bfad503
+size 415
