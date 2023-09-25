d6016206790db448ba6d1d391a603eadba0876d1
김현종 <dw3510@naver.com>
2023-09-25 12:21:16 +0900

    Feat : rent page ui kmm으로 재구성.
    Former-commit-id: de0c6599e547be5a955afed1aec23829388e488f

--- jutopia_kmm/shared/src/commonMain/kotlin/App.kt
+++ jutopia_kmm/shared/src/commonMain/kotlin/App.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90783b1c5c2487d9cf88c8791c614777e00a7946add00a161bc70981cbb29d1a
-size 1721
+oid sha256:7a49b7c8be9a6d00a32e48c2b3eb9352f5a8bb0c718a1e049e6060e6bf33d89f
+size 1870
---
+++ jutopia_kmm/shared/src/commonMain/kotlin/lease/LeasePage.kt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ee242a79e3df48287fc8f6b28712243e4efaaabd916c6b65c2414756e384d2f6
+size 7305
