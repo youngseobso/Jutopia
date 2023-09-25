17b1896bac5949d2da57f64c636134fca9ded233
dhkim6956 <dhkim6956@naver.com>
2023-09-25 20:51:55 +0900

    📝Fix: iOS formatter
    Former-commit-id: 85957d149255798a6c8b24a70d868793215bfb52

--- jutopia_kmm/shared/src/commonMain/kotlin/asset/Asset.kt
+++ jutopia_kmm/shared/src/commonMain/kotlin/asset/Asset.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77cfb431d28aa229f0c9b29c51e15d909af8b076162cdf03d62febd0566cc7f4
-size 4134
+oid sha256:d19777494b34ab0fde99c335fce2acc66d003a77a24135c92e666e53f734166a
+size 4096
