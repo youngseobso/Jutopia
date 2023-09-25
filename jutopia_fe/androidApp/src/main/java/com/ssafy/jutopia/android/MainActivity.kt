6b760bfeabbf5d8a251c175c95fbcb6b8f80ef9d
im jun hwan <dlawnsghks12@naver.com>
2023-09-25 09:31:10 +0900

    📰Feat: bottom tabbar navigation
    Former-commit-id: a60e1095d58775cd26add4bd920a7b1bff3bed25

--- jutopia_fe/androidApp/src/main/java/com/ssafy/jutopia/android/MainActivity.kt
+++ jutopia_fe/androidApp/src/main/java/com/ssafy/jutopia/android/MainActivity.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7bb134e49efc7f13706a91d72a59886d8192d9273335f5df165cad101a9d785
-size 1380
+oid sha256:52213ff39b0c4687b032e5529921b3e428f0601b8c9b2024d517a53aefb877ab
+size 1403
