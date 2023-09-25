f1f6beb3d5c5e868f6826abd8b055da172457667
kihong2424 <kihong2424@gmail.com>
2023-09-25 12:16:18 +0900

    🔈 chore: 도커 로깅 자동화
    Former-commit-id: 4064c4ecb3dabd0cc571cdc1408d587358770553

--- jutopia/sync_logs.sh
+++ jutopia/sync_logs.sh
@@ -1,4 +1,8 @@
-
-while inotifywait -e modify /var/lib/docker/containers/*/*.log; do
+#!/bin/bash
+while true; do
+    inotifywait -e modify /var/lib/docker/containers/*/*.log 2>>/tmp/inotify_errors.log
+    if [ $? -ne 0 ]; then
+        echo "inotifywait exited with error code $?" >> /tmp/inotify_errors.log
+    fi
     rsync -av /var/lib/docker/containers/*/*.log /home/ubuntu/S09P22C108/jutopia/shared/logs/
 done
