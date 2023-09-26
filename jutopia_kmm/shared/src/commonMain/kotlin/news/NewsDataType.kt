f97bc79608063ff2f54b2343233b1e20fc5557f2
dhkim6956 <dhkim6956@naver.com>
2023-09-27 05:28:18 +0900

    📝Test: news url
    Former-commit-id: 391e6fc5c99647fa7153fedef363dd974ebad7b3

--- jutopia_kmm/shared/src/commonMain/kotlin/news/NewsDataType.kt
+++ jutopia_kmm/shared/src/commonMain/kotlin/news/NewsDataType.kt
@@ -2,4 +2,4 @@ package news
 
 data class NewsCategory(val idx: Int, val brand: String, val imgSrc: String)
 
-data class NewsDetail(val title: String, val publisher: String, val date: String, val time: String)
+data class NewsDetail(val title: String, val publisher: String, val date: String, val time: String, val link: String)
--- jutopia_kmm/shared/src/commonMain/kotlin/news/NewsViewModel.kt
+++ jutopia_kmm/shared/src/commonMain/kotlin/news/NewsViewModel.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:531c7f8eafcd8133f3a6753cad521316011f669c3531467c5bf3c6fff434ec7b
-size 651
+oid sha256:445a10d8c6252fddf975ffdd76e2bbb9ec528cc38bce600ad7a32272548059a2
+size 751
