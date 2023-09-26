8fe18984bf8e59dec2dfeae0ff7cb3f06903b6cb
dhkim6956 <dhkim6956@naver.com>
2023-09-27 05:28:18 +0900

    📝Test: news url
    Former-commit-id: 9db82ae799805598853488453639780b449c98fd

--- jutopia_kmm/shared/src/commonMain/kotlin/news/News.kt
+++ jutopia_kmm/shared/src/commonMain/kotlin/news/News.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e799ccd1947860186b5f6353051bb53b87fdb0c04dba26ef305b3ebfb0393416
-size 5911
+oid sha256:6e92a61203d093adcea9fb9f3c3ee0584d37dbfff35c7e872d0106fc875a02c3
+size 6277
