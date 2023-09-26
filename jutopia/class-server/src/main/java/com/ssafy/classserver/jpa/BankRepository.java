37d29d5e212c5d64f8e2d460d373a638ab0d81d0
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-09-26 23:04:11 +0900

    🍃 chore: pykrx 라이브러리 정보 json 파싱 ipynb 파일 생성
    Former-commit-id: 4718af0ffe34df19c2c0e2a3884a391cf9dca364

--- jutopia/class-server/src/main/java/com/ssafy/classserver/jpa/BankRepository.java
+++ jutopia/class-server/src/main/java/com/ssafy/classserver/jpa/BankRepository.java
@@ -1,12 +1,3 @@
-package com.ssafy.classserver.jpa;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.UUID;
-
-public interface BankRepository extends CrudRepository<BankEntity, UUID> {
-    Iterable<BankEntity> findAll();
-    BankEntity findByClassId(UUID classId);
-
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:dd096b999659ffa22a44fb91acb1f177f1ae03ff585aef267e88f84ccfbac2a1
+size 279
