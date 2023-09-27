181cb7836dd0f9375fe01a27ef8e29a73b60f2ff
신성주 <hans52410537@gmail.com>
2023-09-27 11:56:21 +0900

    📝Fix: member-server
    Former-commit-id: c83f49a1d5f845660450b3509cd7d5b8228a0b1e

--- jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/security/SecurityConfig.java
+++ jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/security/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4615053bb7aaa1c2adf4e154e78fa3f1dfbab9ef546ee3fe72a27afc49d96bc0
-size 2151
+oid sha256:a7296ea7ec31f9a7b6c86b6b01fb775df08db1e5937349f448daeca60d32f0ef
+size 2246
