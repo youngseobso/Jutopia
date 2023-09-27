5fd805ffd7b9e8be289c535520dcba130b1a75dd
신성주 <hans52410537@gmail.com>
2023-09-27 17:22:33 +0900

    📝Feat: stock entity
    Former-commit-id: 8853282f662a6dfd282b7dc94a00539329b58b58

--- jutopia/stock-server/src/main/java/com/ssafy/stockserver/domain/trading/entity/Trading.java
+++ jutopia/stock-server/src/main/java/com/ssafy/stockserver/domain/trading/entity/Trading.java
@@ -1,24 +1,3 @@
-package com.ssafy.stockserver.domain.trading.entity;
-
-import jakarta.persistence.*;
-import lombok.*;
-
-import java.math.BigDecimal;
-import java.time.LocalTime;
-
-@AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Getter
-@Builder
-@Entity
-public class Trading {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private BigDecimal price;
-    private LocalTime tradeAt;
-
-    //@ManyToOne
-    //    private Student student;
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:b17e59e0be228ec2c6ecc2fcd063feb1271e4bf47b732c739eab0b9747b858cc
+size 534
