28f729dae858f8b6afe79f5c930d9032a59aef59
bellring <starringbell@gmail.com>
2023-09-28 23:12:07 +0900

    Revert "Revert "Feat : 챗본 페이지를 위한 기본 구성""
    This reverts commit 9709a0aa27f6ea080ec7154fa86d56add165d1e1 [formerly 025030d9610bca56c3594efbd09bd82a5bdd87c1].
    
    
    Former-commit-id: 32fd02ee222a834faba3d4134ef5938e76ea9c4f

---
+++ jutopia_kmm/shared/src/commonMain/kotlin/chatbot/ChatbotPage.kt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:13328bfa81fad58e634612d9d7bd8bd88daa8f60c9c9db778e7c2cef597849c0
+size 2010
---
+++ jutopia_kmm/shared/src/commonMain/kotlin/chatbot/ChatbotViewModel.kt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e5d31514a0beb295d48f814f3f0cf9581df5f0caa0c9587834c24fcf5a89b210
+size 675
