76817b8ab07dbfa72c21b04817c709fa62244733
신성주 <hans52410537@gmail.com>
2023-09-28 17:31:51 +0900

    📝Test: jenkins test
    Former-commit-id: b8de66a9c886e29da24229d7ae2ce4d0ef362d2b

--- jutopia/class-server/src/main/java/com/ssafy/classserver/controller/bank/ProductsController.java
+++ jutopia/class-server/src/main/java/com/ssafy/classserver/controller/bank/ProductsController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d186dcdb1391cfbc9c7f2c12e6aeb541aab848087a1d9af45bd134dbdea09c27
-size 2592
+oid sha256:024352bad92c5538a3410cd832f71cde03a7d136b346d9e0cf812030dc457ec2
+size 2596
