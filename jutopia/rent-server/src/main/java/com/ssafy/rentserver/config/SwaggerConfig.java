5f3bf25523c86ee71944c254ca00394d2a176cc2
bellring <starringbell@gmail.com>
2023-09-28 21:51:51 +0900

    Feat : 좌석 생성 로직변경, swagger에 sanke case 적용, feign 으로 user-server와 통신 준비
    Former-commit-id: 4465b13b15ff464914690cc9842a4f00743e4b87

--- jutopia/rent-server/src/main/java/com/ssafy/rentserver/config/SwaggerConfig.java
+++ jutopia/rent-server/src/main/java/com/ssafy/rentserver/config/SwaggerConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:725219e8106262cb160e1f2c3b36ba75187fc4001fe5bf9b9fb1e3276491435a
-size 857
+oid sha256:59d9fcaffb1d4aa49940c62b8816b588cb9ec87563844ea25f1e114986729d87
+size 1213
