bc9eb21cda3c51a3602e9e58fe366955ac92f421
신성주 <hans52410537@gmail.com>
2023-09-28 20:24:27 +0900

    📝stock: serv
    Former-commit-id: 571cc86d67e0188e549302a5f29eccc4bb926793

--- jutopia/stock-server/src/main/java/com/ssafy/stockserver/domain/stock/entity/Stock.java
+++ jutopia/stock-server/src/main/java/com/ssafy/stockserver/domain/stock/entity/Stock.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3cf080fe9f86d78ef439f6c105493adea4e00addb069cb243e2b7e656f3c8b1
-size 758
+oid sha256:e7d897e8734441f9b0b078036a58a25ad9c43a4f8eeb637008603cf48712cefb
+size 700
--- jutopia/stock-server/src/main/java/com/ssafy/stockserver/domain/trading/entity/Trading.java
+++ jutopia/stock-server/src/main/java/com/ssafy/stockserver/domain/trading/entity/Trading.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b17e59e0be228ec2c6ecc2fcd063feb1271e4bf47b732c739eab0b9747b858cc
-size 534
+oid sha256:a3206cf9a02148a103b26f3dde85ec88e5d7f115a1e83d4544c37ba644bb5bab
+size 720
