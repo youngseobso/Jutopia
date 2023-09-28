84c2c42873a12054bbaf40a7d8a65905b46acd09
Young Seob So <9412sys@gmail.com>
2023-09-28 19:49:25 +0900

    🚑fix: build.gradle crush
    Former-commit-id: c6739946905f0a8055f4089f49e340128133ed15

--- jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/security/SecurityConfig.java
+++ jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/security/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7296ea7ec31f9a7b6c86b6b01fb775df08db1e5937349f448daeca60d32f0ef
-size 2246
+oid sha256:f0edab7766dc60f20f85c90f31eb4c23a51a579d104bc3a94784155425bcb015
+size 2781
