2641e116dad9669f6d61fb1fdf09347d198d5bd5
Young Seob So <9412sys@gmail.com>
2023-09-29 04:11:56 +0900

    🎨fix: studentInfoResponse 값 수정
    Former-commit-id: 381fd61ea17d572025682cbb9930f51c44f2057c

--- jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/students/dto/response/StudentDeleteResponse.java
+++ jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/students/dto/response/StudentDeleteResponse.java
@@ -1,9 +1,3 @@
-package com.ssafy.memberserver.domain.students.dto.response;
-
-public record StudentDeleteResponse(
-        boolean StudentDeleteResult
-) {
-    public static StudentDeleteResponse of(boolean StudentDeleteResult){
-        return new StudentDeleteResponse(StudentDeleteResult);
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:cb4c3145ff7d68d5974f66afd4360422493ef80899c72590c2af5b8393f01f71
+size 283
--- jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/students/dto/response/StudentInfoResponse.java
+++ jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/students/dto/response/StudentInfoResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:461db8df1c9e46ec986326a8899bf556d65dce987d0ef4de45bde63889332f6b
-size 2686
+oid sha256:d5f376dfbcf63e862e9d3d45448a2e407393744887d90c01a1eddbb476f4fe91
+size 2690
