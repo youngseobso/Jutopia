ddf6ebb16eb10d3b5041649f3ff70207c370a3e6
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-09-29 22:08:28 +0900

    🩹 Fix: import 오타 수정
    Former-commit-id: 8227c93731e462e59c67e0f63294b17aa29baa77

--- jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/notice/dto/response/NoticeDeleteResponse.java
+++ jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/notice/dto/response/NoticeDeleteResponse.java
@@ -1,3 +1,9 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:35a4acd2420509ba730a61d254bb8d0dcc5cb5b1aca4ec2e22db5eeffce7b398
-size 275
+package com.ssafy.memberserver.domain.notice.dto.response;
+
+public record NoticeDeleteResponse(
+        boolean noticeDeleteResult
+) {
+    public static NoticeDeleteResponse of(boolean noticeDeleteResult){
+        return new NoticeDeleteResponse(noticeDeleteResult);
+    }
+}
