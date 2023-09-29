967d05d0a1f8d6f04711314f9df5d75b1411ab58
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-09-29 21:31:12 +0900

    🎉 feat: chat-server 업로드
    Former-commit-id: 49a2ad3ca2ff0b225bb279f2444afb3d294b49c3

--- jutopia/class-server/src/main/java/com/ssafy/classserver/service/BankService.java
+++ jutopia/class-server/src/main/java/com/ssafy/classserver/service/BankService.java
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c43156202d4bb876ce76805ac42536bb2a04b65b8ece4da713bc1ea6830dbb9d
-size 322
+package com.ssafy.classserver.service;
+
+import com.ssafy.classserver.dto.ProductDto;
+import com.ssafy.classserver.jpa.BankEntity;
+
+import java.util.UUID;
+
+public interface BankService {
+    Iterable<BankEntity> getAllBanks();
+    BankEntity getClassBanks(UUID classId);
+    ProductDto createProduct(ProductDto product);
+}
