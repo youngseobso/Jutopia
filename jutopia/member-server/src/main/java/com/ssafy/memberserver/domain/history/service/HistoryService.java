773884dfc3f8e85ef586d3865ed9a71df5f4b2e5
소영섭 <persona9412@gmail.com>
2023-09-30 19:15:42 +0900

    🎨fix: member entity and member info
    Former-commit-id: b3aec9cffd9a74af436d365917880f13fefe3e4d

--- jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/history/service/HistoryService.java
+++ jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/history/service/HistoryService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05a196b12b97e6b8c1e6c9a0ae759aff07429e9f4ad07b1b472fd7d3d9588f71
-size 1564
+oid sha256:fe3f35bb9792834814cdfdacb32942b55988bac7a760916ff15fc1c3d1fb176e
+size 3898
