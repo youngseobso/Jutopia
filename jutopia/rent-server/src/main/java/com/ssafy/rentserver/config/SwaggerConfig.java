b813a305166f3af598167cd265c7d16e5b412549
bellring <starringbell@gmail.com>
2023-09-30 12:08:14 +0900

    Fix : rent server api 주소 변경
    Former-commit-id: 9448da1ecb23eb9e93af94a8428a16dcd044aa43

--- jutopia/rent-server/src/main/java/com/ssafy/rentserver/config/SwaggerConfig.java
+++ jutopia/rent-server/src/main/java/com/ssafy/rentserver/config/SwaggerConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59d9fcaffb1d4aa49940c62b8816b588cb9ec87563844ea25f1e114986729d87
-size 1213
+oid sha256:025822e15f338ae3eb07ec6739be0af71585b84dd6df2ac12df4eebaec26638f
+size 1226
