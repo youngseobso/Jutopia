51056763e8315f58f29fd04e80c93413d770ef20
dhkim6956 <dhkim6956@naver.com>
2023-10-01 01:09:50 +0900

    📰Feat: news direction link
    Former-commit-id: 30775c2a1d803715cb2993de75df2b704158d807

--- jutopia_kmm/shared/src/androidMain/kotlin/main.android.kt
+++ jutopia_kmm/shared/src/androidMain/kotlin/main.android.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69b762b09bea9c068f43a801edcd2528cf48d0f196ab763e5fc0aa2c5d3f8633
-size 991
+oid sha256:ed13ea8dbe6c4f8f14723d30ba502ec706d8b4ca659bc94bdf2f9b15f04522ed
+size 1548
--- jutopia_kmm/shared/src/commonMain/kotlin/App.kt
+++ jutopia_kmm/shared/src/commonMain/kotlin/App.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77d3b6678ad790b8d281c818bfd7e2f9d01312fd696d6cbdfd7c309fb120cc7c
-size 4066
+oid sha256:4ccb960dd8f672172296027eb9254102d2b31bd9e9c16a799b9f646cc9647b84
+size 4100
