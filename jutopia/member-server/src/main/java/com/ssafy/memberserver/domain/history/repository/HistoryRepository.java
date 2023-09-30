9ca618c5479cf5ab47171478d506cdb00dcafd43
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-10-01 03:21:01 +0900

    🚧 Chore: 실시간 차트 Scraper (EC2에서 작동 안하는 현상 수정중)
    Former-commit-id: 1e2ed16fe30e3cbe553b451df780c40a9e0ce90b

--- jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/history/repository/HistoryRepository.java
+++ jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/history/repository/HistoryRepository.java
@@ -1,12 +1,3 @@
-package com.ssafy.memberserver.domain.history.repository;
-
-import com.ssafy.memberserver.domain.history.entity.History;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-
-public interface HistoryRepository extends JpaRepository<History,Long> {
-    List<History> findByAccountId(UUID accountId);
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:e5406fb5b61178c49ec08519e7ab1b167767928517b832486b97695c955c01d9
+size 383
