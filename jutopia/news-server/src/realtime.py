fbd207dff881846553763bfeed4522b4256f38ae
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-10-01 17:38:51 +0900

    🩹 Fix: 변수명 오타 수정
    Former-commit-id: 9ce67fbd4073f8a11ff9144fa233c445774e73fe

--- jutopia/news-server/src/realtime.py
+++ jutopia/news-server/src/realtime.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6323dcdd9b219e7cc70087cd8d3c0fb2ff1fc1ca8c4532a25caf21a29036a2f
-size 2698
+oid sha256:38af5c31c4367b5f853ecaf214c77a64744b6b693ebd5ceba01ac74e57c7b87d
+size 2697
