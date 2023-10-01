8843346cd3d6b01f0f702951acf0ba7eccb981dc
bellring <starringbell@gmail.com>
2023-10-01 22:38:22 +0900

    Fix : api 경로 변경
    Former-commit-id: b70299c4b7c1d93a501ac0ab0b4936346ad436bc

--- jutopia/rent-server/src/main/java/com/ssafy/rentserver/controller/SeatController.java
+++ jutopia/rent-server/src/main/java/com/ssafy/rentserver/controller/SeatController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe3db234c07374a0d9fe08151a36cdffbe54fcba4b86cdb9c1b4948c69793bc8
-size 2307
+oid sha256:5d76bea88845670d7316c914c302ceba220399bddbaea4223baedd6cb837f749
+size 2318
