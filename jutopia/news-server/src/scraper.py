00fb56c119650104f0546c3371a65005ad98ac2b
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-10-01 16:48:28 +0900

    🚑 Fix: 실시간 데이터 MongoDB 연결
    Former-commit-id: 0873dd214d20229e6c3e16e69362e2f53a40ae33

--- jutopia/news-server/src/scraper.py
+++ jutopia/news-server/src/scraper.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:17e9fb1e35ef62e4847845c83a612324208f9a01ab3ffed3f36f43e32ac25b1f
-size 4528
+oid sha256:93e5c9cc0624630636fd7f3dc5f8301d695483707a6ce3b6a4b3ff529cd5741c
+size 4858
