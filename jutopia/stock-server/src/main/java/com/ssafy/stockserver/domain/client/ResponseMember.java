e80bfb089e79cfd092a9945d6e75809f3d4ee219
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-10-01 15:42:10 +0900

    🐳 Chore: MongoDB 허용 IP 범위 수정
    Former-commit-id: 415d2cd3a5aefbeff2b7da920c62d1df9b4e561b

--- jutopia/stock-server/src/main/java/com/ssafy/stockserver/domain/client/ResponseMember.java
+++ jutopia/stock-server/src/main/java/com/ssafy/stockserver/domain/client/ResponseMember.java
@@ -1,3 +1,15 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:fa2c8dd657e3abf9e8e7844b0f305d0080ed1325aa0bce8365f0f7eba44141a7
-size 305
+package com.ssafy.stockserver.domain.client;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+public class ResponseMember {
+    private String id;
+    private String student_id;
+    private String student_name;
+    private BigDecimal point;
+    private BigDecimal money;
+}
