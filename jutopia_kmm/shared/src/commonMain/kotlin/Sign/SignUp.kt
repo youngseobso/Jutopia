217d6daca6f95190926fc1f19cbdde6a329a7158
im jun hwan <dlawnsghks12@naver.com>
2023-10-01 20:57:33 +0900

    📰Feat: send detail service
    Former-commit-id: b20605435b8c4816fe7f7f1aa5031a795956caed

---
+++ jutopia_kmm/shared/src/commonMain/kotlin/Sign/SignUp.kt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8b1d3eefbac5d423b6ef5ae3d2855600241a7d1bbc47a90aacade936aabdb361
+size 14994
--- jutopia_kmm/shared/src/commonMain/kotlin/home/Home.kt
+++ jutopia_kmm/shared/src/commonMain/kotlin/home/Home.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:116550d9c1df7ff2dfd6297afe6b1e8e236a0c3e32c72ddd137e9bff50993523
-size 10511
+oid sha256:fe14887114e6208474d9cb84b194fe8c707364a9f5d65f161961e547e19f21d6
+size 10468
