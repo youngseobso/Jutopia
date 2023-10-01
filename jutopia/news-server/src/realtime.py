1196b3befccdcde65d4dd5e3ae056173f7951d31
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-10-02 01:06:23 +0900

    📝 Feat: stocks/ticker API 추가
    Former-commit-id: 77e43eb5a6961633e53ac227f2c40cbb5d3a30f0

--- jutopia/news-server/src/realtime.py
+++ jutopia/news-server/src/realtime.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb6be40eb886cf4e43e5e88ae1ffa59793e214196045fc5cc14aad16c66444c2
-size 4911
+oid sha256:affd683a372d5af4d86fcac79ee13da831fd1b3f3add2f4067d7337416c9c242
+size 6641
