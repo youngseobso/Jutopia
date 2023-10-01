95bc34e309ee5ed75ee5298ecd7ba2d28baf61d9
신성주 <hans52410537@gmail.com>
2023-10-01 16:44:40 +0900

    📝Feat: 회원 적금 가입
    Former-commit-id: f1a940220e532b2dda96d10824dd5f8bc1bc41ca

---
+++ jutopia/class-server/src/main/java/com/ssafy/classserver/jpa/repository/MemberSavingRepository.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:45abfd92baeea221bdb42643c79d210a9dfae5aedffa80ed7f08797be96d9d2d
+size 377
---
+++ jutopia/class-server/src/main/java/com/ssafy/classserver/vo/request/RequestMemberSaving.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:39885f3f7cecb05841efc73df93e8ee50b7692d38b0f6ca02215a614f4746278
+size 398
