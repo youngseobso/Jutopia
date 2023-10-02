c8a6015aa1d88eaa95dcdc467be99ef9fb88d6f4
소영섭 <9412sys@gmail.com>
2023-10-03 01:25:55 +0900

    fix:login
    Former-commit-id: 96f74b325e45457cb33e642818296237e4e162ab

--- jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/students/sign/service/StudentSignService.java
+++ jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/students/sign/service/StudentSignService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e0058843d3c43c17b7c657b28603d5a07a08a7599fb3bedc0bf80a8cd567651
-size 3972
+oid sha256:8b75a0f39dea191c72a3ab13b3ffd2f1dbc19a044f3b43be0023b0d5f58ebf75
+size 4162
