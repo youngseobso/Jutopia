e1eefb7b7836690be6489575fc2c54f3fcf578c4
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-10-02 16:09:37 +0900

    📝 Chore: 차트 API 컬럼 명 변경
    Former-commit-id: 4b5b2a1716724b1d1b6d4fd0a7f6cd8d099bf232

--- jutopia/news-server/src/realtime.py
+++ jutopia/news-server/src/realtime.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9760109c0c0b5229c278afcb1a6692a34d017e369fbacb092de8da4c944a05b0
-size 7688
+oid sha256:4753ab1066b8490698b84cc4a16c557f54d9c32faf83b642f0a4bcba4999eea3
+size 8329
