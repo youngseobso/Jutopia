db0e3c54d2d913a1e1ca56b86d90ab6aaa064d34
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-10-02 16:17:29 +0900

    🩹 Fix: df 변수명 오류 수정
    Former-commit-id: 969dabf9c46c5b5f6d3df98347c873459766d749

--- jutopia/member-server/src/main/java/com/ssafy/memberserver/common/api/ApiStatus.java
+++ jutopia/member-server/src/main/java/com/ssafy/memberserver/common/api/ApiStatus.java
@@ -1,3 +1,5 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:69817e1fd7a593112bda5623648b0e4809459661d2a3aa9d5d901aeb7f0e710b
-size 88
+package com.ssafy.memberserver.common.api;
+
+public enum ApiStatus {
+    SUCCESS,ERROR
+}
--- jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/students/sign/dto/signIn/StudentSignInRequest.java
+++ jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/students/sign/dto/signIn/StudentSignInRequest.java
@@ -1,3 +1,7 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f3faa80622f2eda099908341b21e5f73f754361d1d862206a853c7220220a96c
-size 159
+package com.ssafy.memberserver.domain.students.sign.dto.signIn;
+
+public record StudentSignInRequest(
+        String StudentId,
+        String StudentPwd
+) {
+}
