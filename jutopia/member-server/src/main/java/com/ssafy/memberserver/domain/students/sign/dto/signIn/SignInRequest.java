dd96d10a473d7bd104560f17a154398daa4fe303
소영섭 <9412sys@gmail.com>
2023-10-03 01:40:32 +0900

    fix: member
    Former-commit-id: 528737d9ec37f8269445a843ea79681d4c49b861

--- jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/students/sign/dto/signIn/SignInRequest.java
+++ jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/students/sign/dto/signIn/SignInRequest.java
@@ -1,3 +1,9 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a1813396ae8513f345e1525af404c1f92650147e01318390fb3dce6ed49d9010
-size 251
+package com.ssafy.memberserver.domain.students.sign.dto.signIn;
+
+import lombok.Getter;
+
+@Getter
+public class SignInRequest {
+    private String memberId;
+    private String memberPwd;
+}
