3574591dd485f403ed9134f62f2fe773fcd792d0
소영섭 <9412sys@gmail.com>
2023-10-03 01:40:32 +0900

    fix: member
    Former-commit-id: 0b927a89196181bc972679554455864bc09d2e66

--- jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/students/repository/StudentRepository.java
+++ jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/students/repository/StudentRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9837ac9ed8402ea031a8268d8cb509cc58789f47fb71566616e02a747a7744f2
-size 776
+oid sha256:e3d770487e1d4cd9214c321181ef4516b6fdb457f01a39c43b7cc0262dc15973
+size 775
