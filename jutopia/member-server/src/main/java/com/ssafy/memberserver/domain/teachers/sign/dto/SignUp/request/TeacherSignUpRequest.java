aa6e13e20386ec1444ffa2c1065f7c116250bf01
소영섭 <9412sys@gmail.com>
2023-10-03 04:12:58 +0900

    fix:member
    Former-commit-id: e6ce76e57fe97481f89f496cb6f12d2e232d9333

--- jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/teachers/sign/dto/SignUp/request/TeacherSignUpRequest.java
+++ jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/teachers/sign/dto/SignUp/request/TeacherSignUpRequest.java
@@ -1,18 +1,3 @@
-package com.ssafy.memberserver.domain.teachers.sign.dto.SignUp.request;
-
-import com.ssafy.memberserver.common.enums.MemberBioStatus;
-import com.ssafy.memberserver.common.enums.MemberRole;
-import com.ssafy.memberserver.common.enums.MemberStatus;
-
-import java.util.UUID;
-
-public record TeacherSignUpRequest(
-        String teacherId,
-        String teacherPwd,
-        String Name,
-        String teacherEmail,
-        String school,
-        int grade,
-        int classroom
-) {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:1a8ec4c7f89f455f6a3b36b12d2f3f5b5d2e0e505b9a48f837a9e993993240bc
+size 463
