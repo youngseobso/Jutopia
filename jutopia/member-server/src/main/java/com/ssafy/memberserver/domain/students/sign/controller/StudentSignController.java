b268c940ae4876f11ee627fa71a4d1d52f922bec
소영섭 <9412sys@gmail.com>
2023-10-02 18:16:09 +0900

    fix response
    Former-commit-id: caef57ae8194c02f00e918ae694443337a843f20

--- jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/students/sign/controller/StudentSignController.java
+++ jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/students/sign/controller/StudentSignController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e39a891de2007fa57e88ea80468bc0fee07e2bfe0a56f97e1e2d0100b9350dd
-size 1797
+oid sha256:069b910a8a6ded44ac1a08a1360760c95c9605b282a7ca6d3f66d206b5c3505f
+size 2271
