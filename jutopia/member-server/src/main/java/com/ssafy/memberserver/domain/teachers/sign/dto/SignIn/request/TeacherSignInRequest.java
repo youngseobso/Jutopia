21d617399c348fd5d66c2b2aacca68cfee8e79c3
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-10-03 01:27:14 +0900

    🩹 Fix: timezone 오류 수정
    Former-commit-id: 7863e66fb31bb714d65dd1bd9bf14dc05102552e

--- jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/teachers/sign/dto/SignIn/request/TeacherSignInRequest.java
+++ jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/teachers/sign/dto/SignIn/request/TeacherSignInRequest.java
@@ -1,9 +1,3 @@
-package com.ssafy.memberserver.domain.teachers.sign.dto.SignIn.request;
-
-public record TeacherSignInRequest(
-        String teacherId,
-        String teacherPwd,
-        String teacherName,
-        String email
-) {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:c3739de4e5645109c11ec11b50dd125c4fb91d9641c415bad82be79955df856b
+size 217
