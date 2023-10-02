4828300fb889c63202149d31824dfdb56b81fa75
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-10-02 15:49:47 +0900

    Feat: 실시간 차트 API hour 수정
    Former-commit-id: 25b1b5ce452aef8a4b30a10f5fc10e979156ef83

--- jutopia/news-server/src/realtime.py
+++ jutopia/news-server/src/realtime.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:affd683a372d5af4d86fcac79ee13da831fd1b3f3add2f4067d7337416c9c242
-size 6641
+oid sha256:9760109c0c0b5229c278afcb1a6692a34d017e369fbacb092de8da4c944a05b0
+size 7688
