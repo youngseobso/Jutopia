1323e228949ea7ff4db1afe6cc04a373701b10ea
신성주 <hans52410537@gmail.com>
2023-10-03 02:14:05 +0900

    📝Fix: member
    Former-commit-id: d536f07360a00b1b016d1cc91d0453ce8233826b

--- jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/students/sign/service/StudentSignService.java
+++ jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/students/sign/service/StudentSignService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82cd2a0b0d8bab4b2246303c9de91fb5512e36327f7f82709a6aaaa5137bd607
-size 4223
+oid sha256:3cc196bd09a3097d2ab17cd938f91927449e1f92344649ed27922000c3bc4afb
+size 4225
