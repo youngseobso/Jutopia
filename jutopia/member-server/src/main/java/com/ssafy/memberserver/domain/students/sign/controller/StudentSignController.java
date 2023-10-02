632752499efdd61decea84743521237a8a11686b
신성주 <hans52410537@gmail.com>
2023-10-03 01:31:51 +0900

    📝Feat: user login
    Former-commit-id: 72a932660aaea29ba204645f57872751dae90aef

--- jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/students/sign/controller/StudentSignController.java
+++ jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/students/sign/controller/StudentSignController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ebbb362373817a8c2b7cfaf47f22374ab922336aa7f965263da8e57271a5099e
-size 1840
+oid sha256:505be1649155f606406818ead0da9af447b24e91f5d375efc0888afac2e928c6
+size 2498
