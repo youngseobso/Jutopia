e6556e79cc35b8396c8c7da83250249642aeac62
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-10-02 23:32:04 +0900

    📝 Chore: 차트 API day 받는 값 변경
    Former-commit-id: 1089dc04873e2dceb37e65cc2376f381cc1a0bb4

--- jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/security/AuthenticationSuccessHandler.java
+++ jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/security/AuthenticationSuccessHandler.java
@@ -1,4 +1,3 @@
-package com.ssafy.memberserver.domain.security;
-
-public class AuthenticationSuccessHandler {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:0443ff657f9a0164e77b7fde7d2aed5c6cdcb7f5801cd92953c0bbbfc5e2091a
+size 95
