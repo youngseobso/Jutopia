0bbb95ec8c611fa176acff920c69a8b93cce90a1
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-10-02 17:40:26 +0900

    Fix: cleaned_data 변수명 오류 수정
    Former-commit-id: 7cbac681bf2709cb6cf0e6d996a1e976cb969411

--- jutopia/news-server/src/realtime.py
+++ jutopia/news-server/src/realtime.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18b6448d2f91a1994c0e728b784b928b5a184722f6cdd9216813ca736629e0f9
-size 8487
+oid sha256:f7711e7f45d064f4642c1aa25c3758d14e36fbccbce699d88bf445c0a98fc3fc
+size 8506
