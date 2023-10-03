94ceda109830bffd0022a23ef07284ffb19b7cd9
bellring <starringbell@gmail.com>
2023-10-03 22:49:33 +0900

    Fix : 챗봇 주소 수정
    Former-commit-id: 05ba481bbf33551f06025cca005ca1d80ab9b3d8

--- jutopia_kmm/shared/src/commonMain/kotlin/chatbot/ChatMessage.kt
+++ jutopia_kmm/shared/src/commonMain/kotlin/chatbot/ChatMessage.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95bafd6cb8c2330dd4167921f033f3bf8783e60c6da060e1156440add2f65981
-size 412
+oid sha256:cf511e77714a36af7b852097cfc87ad5738db55a09b0af659bf2385e3d798511
+size 406
--- jutopia_kmm/shared/src/commonMain/kotlin/chatbot/ChatbotApiService.kt
+++ jutopia_kmm/shared/src/commonMain/kotlin/chatbot/ChatbotApiService.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7baa6ba551346637fda24a04fa3e7a803e5795a8d0ec7a8271e4fe02d0f97bde
-size 1727
+oid sha256:4a714f24c6ac5f6a37df7f31dca4272bd00101bdaab2fc5fde443ef56529c579
+size 1736
