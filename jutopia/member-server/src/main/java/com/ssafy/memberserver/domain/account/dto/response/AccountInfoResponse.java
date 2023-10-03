8d690af1904a8ec1e2cf05a60bd1c2ab56e56eb1
소영섭 <9412sys@gmail.com>
2023-10-04 01:43:05 +0900

    fix: notice bug
    Former-commit-id: 71ee192b59d3496f17d2299d71c60923391a3480

--- jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/account/dto/response/AccountInfoResponse.java
+++ jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/account/dto/response/AccountInfoResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d54595685c7c526600e93fdc8bf8e3c66da39d8062a0a64094adac94835f5f40
-size 718
+oid sha256:27c6f46d09c36f0fc9cb4f24adeb1c486b67465553fbc2584a25faef2cae0a8e
+size 949
