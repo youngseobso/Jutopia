cff538c109f40293a40a7a7c16c8443188c6f3eb
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-10-03 17:38:04 +0900

    Feat: 뉴스 API
    Former-commit-id: 58da2f03a4cd966e03fcf88a783b79b43b8873f9

--- jutopia/news-server/src/news.py
+++ jutopia/news-server/src/news.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:534da3628d2936a54a19393c4327e1811ed73ccc628ee16dcd1e6125618d74d4
-size 2540
+oid sha256:90e9a4a37089c5a1ba712162e48c7283a6b573d84f97ddad7c1eb0afce04aa1d
+size 2566
