a232acbeb0526bbc05ee111f1ba747e805d95d97
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-10-03 17:11:29 +0900

    chore: 테스트
    Former-commit-id: ecb66c22e6513fd541dab3a003d84ae1d36833f1

--- jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/students/dto/request/MemberPointUpdateRequest.java
+++ jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/students/dto/request/MemberPointUpdateRequest.java
@@ -1,11 +1,3 @@
-package com.ssafy.memberserver.domain.students.dto.request;
-
-import java.math.BigDecimal;
-import java.util.UUID;
-
-public record MemberPointUpdateRequest(
-        UUID id,
-        BigDecimal point
-
-) {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:99307752c3c5d13787f43bcae55f064eabe81a5b3ba21825d815840a9a6e5931
+size 203
