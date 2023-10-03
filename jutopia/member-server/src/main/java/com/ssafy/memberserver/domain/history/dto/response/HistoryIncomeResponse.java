b43d6087b0a3b4bfcd3618ac149a1547b5e93543
소영섭 <9412sys@gmail.com>
2023-10-04 02:09:44 +0900

    fix: notice and history
    Former-commit-id: cc76d3b6fafef979868660e9fa54dfe2cb074666

--- jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/history/dto/response/HistoryIncomeResponse.java
+++ jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/history/dto/response/HistoryIncomeResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2c512d2d5f0cb7587ccd332ea7256897ffa0d6313150ea28a74ab4eabb3ed58
-size 743
+oid sha256:f33ed1be604951f475ee4fc8bea3d1c407d8a4a78e5607de2c56f82450f6d560
+size 885
