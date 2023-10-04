92470a5dc7208e2af140ba28934a776856bb56ee
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-10-04 12:04:27 +0900

    🍃 Chore: 뉴스서버 stocks API 리턴 값 변경
    Former-commit-id: 878531d2a55cff03da4506e8fa17af914a617c21

--- jutopia/news-server/src/realtime.py
+++ jutopia/news-server/src/realtime.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20f3651e87b7c6a39a3bd9c926caaeb5f4b0f5b5901a7cafd8a9564f62f53665
-size 8996
+oid sha256:31a66501ccf41249a8e1893cea1c7e1ad8331280487e72c1c880641374712dfa
+size 11516
