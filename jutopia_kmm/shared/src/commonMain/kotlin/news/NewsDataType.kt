f65daab763b725c648dab4be43732d12ac74e30e
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-10-04 12:04:27 +0900

    🍃 Chore: 뉴스서버 stocks API 리턴 값 변경
    Former-commit-id: 528f7221a1af707a4ac1a5d0ff087b7394d3d32d

--- jutopia_kmm/shared/src/commonMain/kotlin/news/NewsDataType.kt
+++ jutopia_kmm/shared/src/commonMain/kotlin/news/NewsDataType.kt
@@ -1,5 +1,3 @@
-package news
-
-data class NewsCategory(val idx: Int, val brand: String, val imgSrc: String)
-
-data class NewsDetail(val title: String, val publisher: String, val date: String, val time: String, val link: String)
+version https://git-lfs.github.com/spec/v1
+oid sha256:ac81f341540a8089386b7850b1e3bb97a7136f983367f78013c57d29d3f763f7
+size 209
