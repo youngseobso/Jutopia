7daa0ad6da9624d47cefc55127893f2331fdf160
dhkim6956 <dhkim6956@naver.com>
2023-10-05 04:33:50 +0900

    📰Feat: logout
    Former-commit-id: ed15f169b9067b5fca42c9edfbdd9e4c2ec68754

--- jutopia_kmm/shared/src/commonMain/kotlin/menus/Menus.kt
+++ jutopia_kmm/shared/src/commonMain/kotlin/menus/Menus.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:946effc40127722e4dd2cdc8a1af98b253ce7c4417b83664c87ea55c7d8f3fb3
-size 1083
+oid sha256:f711f04ed1809bf7b6a0229bbaa68dd78a92da1d5b15ac4b96497de132211155
+size 5608
