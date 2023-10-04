1ca8eec5a60bc137aa35417f5245780be44701b6
김현종 <dw3510@naver.com>
2023-10-04 10:02:58 +0900

    Fix : 미체결 삭제에 따라 구매,판매 가격 조절 불가로 변경
    Former-commit-id: 8a09c2395748142e46340b14903ed8e1df660cc0

--- jutopia_kmm/shared/src/commonMain/kotlin/stock/stocktrade/StockBuyingPage.kt
+++ jutopia_kmm/shared/src/commonMain/kotlin/stock/stocktrade/StockBuyingPage.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4eeb974dd49cc1b63ef33974f15788bc6fd4dfcae053c73d8671701f95f0a078
-size 7612
+oid sha256:6d6ec9a6e3d5e51455dc168308c7ccdb5aa87963e6f76efcfb5672f54233b93a
+size 7901
