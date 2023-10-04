897d1cf71342c064d210de04e4614813b1214d7d
bellring <starringbell@gmail.com>
2023-10-04 21:24:48 +0900

    Fix : 회원가입 페이지 done 버튼 동작 수정
    Former-commit-id: 45c67e7646d66549c67a1253e5dbbe30d086b4a7

--- jutopia_kmm/shared/src/commonMain/kotlin/Sign/MainPage.kt
+++ jutopia_kmm/shared/src/commonMain/kotlin/Sign/MainPage.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db260d017d8bd2e0f4e5e1c6786765f03f1a6fc7224962a98bdbf5c2c9ddd0c3
-size 8969
+oid sha256:80f4713cb5de24080b6e2ec161602d8bfb360e22b6887fa34fdae31ee195156b
+size 9954
--- jutopia_kmm/shared/src/commonMain/kotlin/Sign/SignUp.kt
+++ jutopia_kmm/shared/src/commonMain/kotlin/Sign/SignUp.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50392374593907f080dc195bed1d6a53067c0d292ec9ac32bcb2abe59126cdd1
-size 15869
+oid sha256:6e00f700213b1b86a3945469ec3d017ae504e251ea59809e79748e2f4c284a2d
+size 17312
