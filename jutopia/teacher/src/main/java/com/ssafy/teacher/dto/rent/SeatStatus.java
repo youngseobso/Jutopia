f6fa25a11353537d0bf1e3669e88ccfa55ae7cab
소영섭 <9412sys@gmail.com>
2023-10-05 00:37:52 +0900

    fix: ssl
    Former-commit-id: 492244734349a7c64ed52f416a7b416e562b7b38

--- jutopia/teacher/src/main/java/com/ssafy/teacher/dto/rent/SeatStatus.java
+++ jutopia/teacher/src/main/java/com/ssafy/teacher/dto/rent/SeatStatus.java
@@ -1,13 +1,3 @@
-package com.ssafy.teacher.dto.rent;
-
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
-public enum SeatStatus {
-    AVAILABLE("사용가능"),
-    UNAVAILABLE("사용불가"),
-    INUSE("사용중"),
-    DELETED("삭제");
-
-    private final String description;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:7fd19ab636ca5cc467b6d21ef33e905a4831df7e35e2f07a6f82a2980a0677c9
+size 269
