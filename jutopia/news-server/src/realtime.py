31f3483841599eb42f6ccc80860687aa7bc86897
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-10-05 03:05:22 +0900

    🚑 Chore: MongoDB 단일DB로 변경
    Former-commit-id: a1d7a20a8561e273cb67b214661755254f4874b4

--- jutopia/news-server/src/realtime.py
+++ jutopia/news-server/src/realtime.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31a66501ccf41249a8e1893cea1c7e1ad8331280487e72c1c880641374712dfa
-size 11516
+oid sha256:bddeeb58dacc9bac45acab44ae3f696b9d95cb443a6b99ebe189a1c236c8b038
+size 9742
