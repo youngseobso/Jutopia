0720896725b78b8c132af5bf8e96de36299a7247
소영섭 <9412sys@gmail.com>
2023-10-04 23:49:24 +0900

    fix: ssl
    Former-commit-id: d6472f879327aeaf917f080fd8215f3d30dc541a

--- jutopia/teacher/src/main/java/com/ssafy/teacher/dto/common/Response.java
+++ jutopia/teacher/src/main/java/com/ssafy/teacher/dto/common/Response.java
@@ -1,3 +1,20 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f99d479c4eb0e6832f0b9c24bc2d6c9a30c491ad5b784034b43676a898b9a617
-size 371
+package com.ssafy.teacher.dto.common;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+@Getter
+public class Response<T> {
+
+    private Result result;
+    private T body;
+
+    @Getter
+    @Setter
+    public static class Result {
+        private int result_code;
+        private String result_message;
+        private String result_description;
+    }
+}
