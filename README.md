fcc4dad921cc6d91d421671ed7c6ba830546b46b
신성주 <hans52410537@gmail.com>
2023-10-05 14:49:59 +0900

    Update README.md
    Former-commit-id: 635ab99a33525f0825656e2f894677408e9b1fb0

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f9e3947a0ef6d748b9f767aacc4fc56af920cc0f4cc420bd0dbc20a87aed34e9
-size 8225
+oid sha256:3a6934dff23062f63df0d5853b0f605622f9fd29591ff7a0cade19bdd8881745
+size 8224
--- jutopia_kmm/shared/src/commonMain/kotlin/asset/subMenu/MySaveViewModel.kt
+++ jutopia_kmm/shared/src/commonMain/kotlin/asset/subMenu/MySaveViewModel.kt
@@ -1,3 +1,11 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6a436e7ffa1c321d505824767bee28a6e710f912c5c4ac9756baf6e49a99c5fc
-size 376
+package asset.subMenu
+
+import moe.tlaster.precompose.viewmodel.ViewModel
+
+class MySaveViewModel: ViewModel() {
+    private val _transactionHistory: MutableList<depositDetail> = mutableListOf(
+        depositDetail("2023.09.07", "15:14:00", transactionType.Deposit, "1회차 입금", 10000, 10000),
+    )
+
+    val transactionHistory: List<depositDetail> = _transactionHistory
+}
