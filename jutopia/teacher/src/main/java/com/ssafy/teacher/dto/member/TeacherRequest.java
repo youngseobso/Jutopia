b62ee67d79b07d6a09a0df40c6a3d6db5e20919a
김현종 <dw3510@naver.com>
2023-10-05 09:53:11 +0900

    Feat : 로그인 페이지
    Former-commit-id: b1a9215c809c6f76cdd483ce773496c2384b7df0

--- jutopia/teacher/src/main/java/com/ssafy/teacher/dto/member/TeacherRequest.java
+++ jutopia/teacher/src/main/java/com/ssafy/teacher/dto/member/TeacherRequest.java
@@ -1,4 +1,3 @@
-package com.ssafy.teacher.dto.member;
-
-public class TeacherRequest {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:145fbd4a4d6344780dc124c90fc10831d8431005a3fec868ed1b8cf8cb39ec69
+size 223
