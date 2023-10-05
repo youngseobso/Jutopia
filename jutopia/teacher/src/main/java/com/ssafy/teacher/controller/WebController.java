2aab9ccfe75808cff227592c8cf6a536985af02a
김현종 <dw3510@naver.com>
2023-10-05 17:39:56 +0900

    Fix : 관리자 페이지 경로 수정
    Former-commit-id: 8c553a5bde030b31d23dabcd073354170783c2fc

--- jutopia/teacher/src/main/java/com/ssafy/teacher/controller/WebController.java
+++ jutopia/teacher/src/main/java/com/ssafy/teacher/controller/WebController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48b40ba56ee16416109f777659e4b02b0e7da8a4feaf58cf2fc70d7368150258
-size 3305
+oid sha256:0f98ba4b2a76f117034700c9ba312a1a4236234e2f4ddc270b4aabba3b06a0f5
+size 3340
