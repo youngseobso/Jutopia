b2345b10544c76db8ed805f9186c4098e4271f49
소영섭 <persona9412@gmail.com>
2023-10-05 10:23:12 +0900

    Update jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/sign/dto/SignInResponse.java
    Former-commit-id: d443fac3ad60c266292c1af99ac772192762c9c3

--- jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/sign/dto/SignInResponse.java
+++ jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/sign/dto/SignInResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afd0da2141a6287d7c025a1a53b9ab10af8d3a320a8fcb9da81c5051e1ede048
-size 2328
+oid sha256:f65ee5525316903bbab8692d58639492feeaea6b39880fdccb3b25c765dbc220
+size 2405
