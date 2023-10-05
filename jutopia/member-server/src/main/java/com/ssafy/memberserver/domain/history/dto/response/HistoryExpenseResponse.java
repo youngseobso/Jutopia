fea76fb7608be12a9e8eaff1beefbc1822bd1a14
Young Seob So <9412sys@gmail.com>
2023-10-05 13:57:39 +0900

    fix: history
    Former-commit-id: f2df99e51feeefcb88c61dcd104921c821e9e193

--- jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/history/dto/response/HistoryExpenseResponse.java
+++ jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/history/dto/response/HistoryExpenseResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2952400a9e17ff3c0a6fdcab97f2a510a68505648a2472c34458eb0b4fd94972
-size 888
+oid sha256:e0de918e2fcf8b1d004f49808c8909f59200a9557f1a2eeff918b1178c23993f
+size 1080
