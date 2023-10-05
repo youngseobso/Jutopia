7200c0ff9feb529bcf89b7d59fb9bdd0893fc894
김현종 <dw3510@naver.com>
2023-10-05 17:51:27 +0900

    Fix : 포트 수정
    Former-commit-id: e3278e33a6f358d9aca4e68aa5cdda85b104dea3

--- jutopia/teacher/src/main/java/com/ssafy/teacher/controller/WebController.java
+++ jutopia/teacher/src/main/java/com/ssafy/teacher/controller/WebController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7df15aaf4a9517010776c5166910ec9508f4f1a825640ef2ee113ae12afac6ae
+oid sha256:30af69ae55ac6bc098fd8107eb7af893a91be0a8dfc71a384e03d6db686935d9
 size 3454
