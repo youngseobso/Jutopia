6cc66b0364f400e428317daa15d474a2d4f3c939
김도훈 <dhkim6956@naver.com>
2023-10-05 13:53:54 +0900

    📰Feat: landing page
    Former-commit-id: e5598ad1dc4f85e2122156e4e65d19a52c082e9e

--- jutopia_kmm/shared/src/commonMain/kotlin/App.kt
+++ jutopia_kmm/shared/src/commonMain/kotlin/App.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a9defab01866bedfc9dfc57b105c4b11e9601910babbb09a5b79db27a456a17
-size 7362
+oid sha256:0dff07f153bb40194d6204c279f4a737f16aca5cfd994f7325e00b584563e366
+size 7523
