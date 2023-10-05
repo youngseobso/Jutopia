0daa16234cfee1e919d47a20a47551bfb479bec3
소영섭 <persona9412@gmail.com>
2023-10-05 11:34:01 +0900

    Update jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/mail/config/MailConfig.java
    Former-commit-id: 00530aa9462646bd39a7efee8d82da79feec26c0

--- jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/mail/config/MailConfig.java
+++ jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/mail/config/MailConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:849ed3effa92f8747507a87ea2b6e0b49afae1375576d75cb741b160e05885a9
-size 1671
+oid sha256:a7a8b0d336f6ad496b79c3b5a0e9dadb0b8a8e7a267b299d0e9b61d3a97b2ab9
+size 1731
