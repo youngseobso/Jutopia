d65a6207fe4b10ce06b7287394e59e0dff5507d0
김현종 <dw3510@naver.com>
2023-10-06 08:50:42 +0900

    Deleted jutopia_fe/androidApp/src/main/java/com/ssafy/jutopia/android/model/Seat.kt, jutopia_fe/androidApp/src/main/java/com/ssafy/jutopia/android/ui/lease/LeasePage.kt, jutopia_fe/androidApp/src/main/java/com/ssafy/jutopia/android/ui/lease/LeaseViewModel.kt, jutopia_fe/androidApp/src/main/java/com/ssafy/jutopia/android/ui/theme/Color.kt, jutopia_fe/androidApp/src/main/java/com/ssafy/jutopia/android/ui/theme/MyApplicationTheme.kt, jutopia_fe/androidApp/src/main/java/com/ssafy/jutopia/android/ui/BankPage.kt, jutopia_fe/androidApp/src/main/java/com/ssafy/jutopia/android/ui/MainPage.kt, jutopia_fe/androidApp/src/main/java/com/ssafy/jutopia/android/ui/MarketPage.kt, jutopia_fe/androidApp/src/main/java/com/ssafy/jutopia/android/ui/NoticePage.kt, jutopia_fe/androidApp/src/main/java/com/ssafy/jutopia/android/ui/StockPage.kt, jutopia_fe/androidApp/src/main/java/com/ssafy/jutopia/android/ui/TradePage.kt, jutopia_fe/androidApp/src/main/java/com/ssafy/jutopia/android/BankActivity.kt, jutopia_fe/androidApp/src/main/java/com/ssafy/jutopia/android/GridviewAdapter.kt, jutopia_fe/androidApp/src/main/java/com/ssafy/jutopia/android/Icon.kt, jutopia_fe/androidApp/src/main/java/com/ssafy/jutopia/android/JutopiaApp.kt, jutopia_fe/androidApp/src/main/java/com/ssafy/jutopia/android/MainActivity.kt, jutopia_fe/androidApp/src/main/java/com/ssafy/jutopia/android/SaveActivity.kt, jutopia_fe/androidApp/src/main/java/com/ssafy/jutopia/android/SendActivity.kt, jutopia_fe/androidApp/src/main/res/drawable/bank.xml, jutopia_fe/androidApp/src/main/res/drawable/deal.xml, jutopia_fe/androidApp/src/main/res/drawable/lease.xml, jutopia_fe/androidApp/src/main/res/drawable/market.xml, jutopia_fe/androidApp/src/main/res/drawable/notice.xml, jutopia_fe/androidApp/src/main/res/drawable/rabbit_coin.xml, jutopia_fe/androidApp/src/main/res/drawable/save.xml, jutopia_fe/androidApp/src/main/res/drawable/send.xml, jutopia_fe/androidApp/src/main/res/drawable/stock.xml, jutopia_fe/androidApp/src/main/res/drawable/trade.xml, jutopia_fe/androidApp/src/main/res/layout/activity_bank.xml, jutopia_fe/androidApp/src/main/res/layout/activity_main.xml, jutopia_fe/androidApp/src/main/res/layout/activity_save.xml, jutopia_fe/androidApp/src/main/res/layout/activity_send.xml, jutopia_fe/androidApp/src/main/res/layout/gridview_item.xml, jutopia_fe/androidApp/src/main/res/layout/main_my_assets.xml, jutopia_fe/androidApp/src/main/res/values/attrs_ui.xml, jutopia_fe/androidApp/src/main/res/values/colors.xml, jutopia_fe/androidApp/src/main/res/values/strings.xml, jutopia_fe/androidApp/src/main/res/values/styles.xml, jutopia_fe/androidApp/src/main/res/values-night/styles.xml, jutopia_fe/androidApp/src/main/AndroidManifest.xml, jutopia_fe/androidApp/build.gradle.kts, jutopia_fe/gradle/wrapper/gradle-wrapper.jar, jutopia_fe/gradle/wrapper/gradle-wrapper.properties, jutopia_fe/iosApp/iosApp/Assets.xcassets/AccentColor.colorset/Contents.json, jutopia_fe/iosApp/iosApp/Assets.xcassets/AppIcon.appiconset/Contents.json, jutopia_fe/iosApp/iosApp/Assets.xcassets/Contents.json, jutopia_fe/iosApp/iosApp/Preview Content/Preview Assets.xcassets/Contents.json, jutopia_fe/iosApp/iosApp/ContentView.swift, jutopia_fe/iosApp/iosApp/Info.plist, jutopia_fe/iosApp/iosApp/iOSApp.swift, jutopia_fe/iosApp/iosApp.xcodeproj/project.pbxproj, jutopia_fe/shared/src/androidMain/kotlin/com/ssafy/jutopia/Platform.android.kt, jutopia_fe/shared/src/commonMain/kotlin/com/ssafy/jutopia/Greeting.kt, jutopia_fe/shared/src/commonMain/kotlin/com/ssafy/jutopia/Platform.kt, jutopia_fe/shared/src/iosMain/kotlin/com/ssafy/jutopia/Platform.ios.kt, jutopia_fe/shared/build.gradle.kts, jutopia_fe/.gitignore, jutopia_fe/build.gradle.kts, jutopia_fe/gradle.properties, jutopia_fe/gradlew, jutopia_fe/gradlew.bat, jutopia_fe/settings.gradle.kts
    Former-commit-id: 148852a4a554f7bc159fb492b188d4629480b53d

--- jutopia_fe/shared/build.gradle.kts
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1feac24311cfffa5139c086ffb3bfe994f8516341ee28e3dc35b60ffba3afe1f
-size 920
--- jutopia_fe/shared/src/androidMain/kotlin/com/ssafy/jutopia/Platform.android.kt
+++
@@ -1,7 +0,0 @@
-package com.ssafy.jutopia
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
