4c4ee599c83d212edd05e5745eb242407b549237
김현종 <dw3510@naver.com>
2023-10-05 17:41:00 +0900

    Fix : school 수정
    Former-commit-id: 4c39305df287098bda47742ece5037cf58bc5966

--- jutopia/teacher/src/main/java/com/ssafy/teacher/controller/WebController.java
+++ jutopia/teacher/src/main/java/com/ssafy/teacher/controller/WebController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f98ba4b2a76f117034700c9ba312a1a4236234e2f4ddc270b4aabba3b06a0f5
-size 3340
+oid sha256:78c4e6cff4a6972e703f2f4926a0b9caef4cfcc5e87ffa9d96d715e920ccd063
+size 3338
