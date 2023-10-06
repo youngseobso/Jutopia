e3e6c21abb51e2c5a42d680ca8a22ee037ae311b
kihong2424 <kihong2424@gmail.com>
2023-10-06 11:13:27 +0900

    🐎 Feat: 챗봇 답변 글자수 250자 제한
    Former-commit-id: cb0c14e49e63544a9407fbd6e7d21f526cff61f7

--- jutopia/chat-server/utils.py
+++ jutopia/chat-server/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5114be0c49f26d1b4394e717353e9cc2b8c1b0ea0d5055a2ff46d0fef65b8240
-size 682
+oid sha256:ae9b964f4f64050767a66528a3371e001e7844f90eb762c9d1dc57683cd919de
+size 5165
