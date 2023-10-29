2b320f8336311bb5f969f275d3b65f8aeb297d48
김기홍 <kihong2424@gmail.com>
2023-10-29 18:18:52 +0900

    📝 Chore : openai api key 삭제
    Former-commit-id: 85b4cbea7b783486359e42dac3126e2030020e83

--- jutopia/chat-server/utils.py
+++ jutopia/chat-server/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64f1bb6dcf8f6d2173dade3b05c23d7835fc057e4e7dd3eaacc7128e6cc7b686
-size 5095
+oid sha256:e10a13b3fc05851fa7db887e69572f0e9e38d3d095936b6bf8db10d756ddcc37
+size 5059
